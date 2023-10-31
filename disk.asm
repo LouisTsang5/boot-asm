f96af9b524edf50f7cc0746541a732e1ea64ec41
louistsang <louis.tsang@ricoh.com.hk>
2023-10-31 17:12:59 +0800

    refactor

--- disk.asm
+++ disk.asm
@@ -1,6 +1,8 @@
 [bits 16]       ; 16 bits mode
 [org 0x7c00]    ; Boot memory starts at offset 7c00
 
+mov [boot_device], dl ; BIOS store boot device to dl on startup
+
 ; Initialize stack pointers to 0x8000
 mov bp, 0x8000
 mov sp, bp
@@ -10,16 +12,34 @@ mov ax, boot_msg
 call print_str
 
 ; Load from disk
-mov bx, 0x9000
-mov dh, 5
-mov dl, 0
+mov bx, 0x9000          ; Load to addr 0x9000
+mov dh, 5               ; Load 5 sectors
+mov dl, [boot_device]   ; Read from boot device
 call load_disk
+
+; Display success message
+push ax
+mov ax, disk_suc_msg_0
+call print_str
+pop ax
+call print_hex
+mov ax, disk_suc_msg_1
+call print_str
 
 ; End of program
 jmp $
 
+boot_device: db 0
+
+; Strings with \r\n\0 at the end
+boot_msg: db 'System Booted.', 13, 10, 0
+disk_err_msg: db 'Failed to read disk.', 13, 10, 0
+disk_suc_msg_0: db 'Successfully read ', 0
+disk_suc_msg_1: db ' sectors from disk', 13, 10, 0
+
 ; Read from target device from second to n+1 sectors to destination
 ; Input - bx: destination pointer, dh: number of sectors to read, dl: device to read from
+; Return - al: number of sectors read
 load_disk:
     push dx ; store dx for comparison later
     
@@ -106,10 +126,6 @@ print_hex_last_bits:
     ret
 hex_digits:
     db "0123456789ABCDEF"
-
-; Strings with \r\n\0 at the end
-boot_msg: db 'System Booted.', 13, 10, 0
-disk_err_msg: db 'Failed to read disk.', 13, 10, 0
 
 ; Padding and magic BIOS number.
 times 510 - ($-$$) db 0
