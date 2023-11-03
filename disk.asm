80218e67f24ae6ad59ace604b42038f9a3a08ee4
louistsang5 <louistsang5@gmail.com>
2023-11-04 00:28:26 +0800

    define constants to hardcoded values

--- disk.asm
+++ disk.asm
@@ -1,10 +1,18 @@
 [bits 16]       ; 16 bits mode
 [org 0x7c00]    ; Boot memory starts at offset 7c00
 
-mov [boot_device], dl ; BIOS store boot device to dl on startup
+; Define Constants
+STACK_ADDR equ 0x8000       ; Addr of stack
+CR equ 13                   ; CR char
+LF equ 10                   ; LF char
+DISK_LOAD_ADDR equ 0x9000   ; Addr to load disk image to
+DISK_LOAD_N_SECTORS equ 5   ; Number of sectors to read
 
-; Initialize stack pointers to 0x8000
-mov bp, 0x8000
+; BIOS stores boot device to dl on startup
+mov [boot_device], dl 
+
+; Initialize stack pointers
+mov bp, STACK_ADDR
 mov sp, bp
 
 ; Print string
@@ -12,9 +20,9 @@ mov ax, boot_msg
 call print_str
 
 ; Load from disk
-mov bx, 0x9000          ; Load to addr 0x9000
-mov dh, 5               ; Load 5 sectors
-mov dl, [boot_device]   ; Read from boot device
+mov bx, DISK_LOAD_ADDR      ; Load to disk addr
+mov dh, DISK_LOAD_N_SECTORS ; Load 5 sectors
+mov dl, [boot_device]       ; Read from boot device
 call load_disk
 
 ; Display success message
@@ -26,16 +34,22 @@ call print_hex
 mov ax, disk_suc_msg_1
 call print_str
 
+; Display loaded values
+mov al, [DISK_LOAD_ADDR]
+call print_hex
+mov al, [DISK_LOAD_ADDR + 512]
+call print_hex
+
 ; End of program
 jmp $
 
 boot_device: db 0
 
 ; Strings with \r\n\0 at the end
-boot_msg: db 'System Booted.', 13, 10, 0
-disk_err_msg: db 'Failed to read disk.', 13, 10, 0
+boot_msg: db 'System Booted.', CR, LF, 0
+disk_err_msg: db 'Failed to read disk.', CR, LF, 0
 disk_suc_msg_0: db 'Successfully read ', 0
-disk_suc_msg_1: db ' sectors from disk', 13, 10, 0
+disk_suc_msg_1: db ' sectors from disk', CR, LF, 0
 
 %include "./disk/load_disk.asm" ; load disk logic
 %include "./print/print_str.asm" ; print str func
